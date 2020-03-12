251cf67852a62fb4c79d059d0d10d2a09693558e
AidanJP <aidanpendlebury@gmail.com>
2020-03-12 22:51:13 +0000

    Syntactic sugar.

--- Todos/ContentView.swift
+++ Todos/ContentView.swift
@@ -8,9 +8,74 @@
 
 import SwiftUI
 
+struct TodoItem: Identifiable {
+    var id = UUID()
+    var description: String
+}
+
 struct ContentView: View {
+    @State private var todos: [TodoItem] = []
+    @State private var showingAddTodoView = false
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            NavigationView {
+                VStack(spacing: 5) {
+                    ForEach(todos) { item in
+                        HStack {
+                            Text(item.description).padding()
+                            Spacer()
+                            Button("Delete") { self.delete(item: item) }.foregroundColor(.white).padding().background(Color.gray)
+                        }.background(Color(UIColor.lightGray))
+                    }
+                    Spacer()
+                }
+                .navigationBarTitle(Text("Todos"), displayMode: .inline)
+                .navigationBarItems(trailing: Button(action: { self.showingAddTodoView = true }) {
+                    Image(systemName: "plus")
+                })
+            }
+            if showingAddTodoView {
+                ZStack {
+                    Color.black.opacity(0.3).edgesIgnoringSafeArea(.all)
+                    AddItemView(todos: self.$todos, showingTodoView: $showingAddTodoView)
+                }
+            }
+        }
+    }
+    
+    func delete(item: TodoItem) {
+        if let index = todos.firstIndex(where: { $0.id == item.id }) {
+            withAnimation { todos.remove(at: index) }
+        }
+    }
+    
+}
+
+struct AddItemView: View {
+    @Binding var todos: [TodoItem]
+    @Binding var showingTodoView: Bool
+    @State private var newItem = ""
+    var body: some View {
+        ZStack {
+            Color.white.shadow(radius: 10, x: 5, y: 5)
+            VStack(alignment: .leading) {
+                Text("Add item").font(.body).fontWeight(.bold).padding()
+                Spacer()
+                TextField("", text: $newItem).background(Color.gray).padding(.horizontal)
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button("OK") { self.saveNewItem() }.foregroundColor(.black).padding()
+                }
+            }
+        }
+        .frame(width: 260, height: 150)
+    }
+    
+    func saveNewItem() {
+        guard !newItem.isEmpty else { return }
+        withAnimation { self.todos.append(TodoItem(description: newItem)) }
+        self.showingTodoView = false
     }
 }
 
